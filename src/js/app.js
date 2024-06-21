2cfbd4639b9da76f4c0be3a2807bc56106b21aef
Jean Carlo Urrego <jctaziturno@gmail.com>
2024-06-21 14:53:45 -0500

    fixed js

--- src/js/app.js
+++ src/js/app.js
@@ -6,12 +6,13 @@ document.addEventListener('DOMContentLoaded',function(){
 
 function darkmode() {
     const prefiereDarkMode = window.matchMedia('(prefers-color-scheme: dark)');
-    //console.log(prefiereDarkMode.matches);
+    
     if(prefiereDarkMode.matches){
         document.body.classList.add('dark-mode');
     }else{
         document.body.classList.remove('dark-mode');
     };
+
     prefiereDarkMode.addEventListener('change',function() {
         if(prefiereDarkMode.matches){
             document.body.classList.add('dark-mode');
@@ -19,6 +20,7 @@ function darkmode() {
             document.body.classList.remove('dark-mode');
         }
     });
+
     const botonDarkMode = document.querySelector('.dark-mode-boton');
 
     botonDarkMode.addEventListener('click', function() {
